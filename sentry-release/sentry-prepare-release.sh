b42dd25e3aaca912ce51bd5161dafcc9757408a8
Arni <toarni@gmail.com>
2024-12-05 23:15:55 +0100

    feat: removing sentry-release, because we don't need it any longer

--- sentry-release/sentry-prepare-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-node /opt/sentryjs/sentry-prepare-release.js
-if [ $? -eq 0 ]; then
-    echo OK
-else
-    exit 1
-fi
--- sentry-release/sentry-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-node /opt/sentryjs/sentry-release.js
-if [ $? -eq 0 ]; then
-    echo OK
-else
-    exit 1
-fi
