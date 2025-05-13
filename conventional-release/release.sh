7c6feb00a8ed2b18011bef85fd4ef6f3985a8b50
Arni <toarni@gmail.com>
2025-05-13 15:35:40 +0200

    fix: allow to use user and app password for git pushback

--- conventional-release/release.sh
+++ conventional-release/release.sh
@@ -5,6 +5,8 @@ echo "Checking environment variables..."
 RELEASE_DIR=${RELEASE_DIR:-$BITBUCKET_CLONE_DIR}
 GIT_PUSH_BACK_OAUTH_ID=${GIT_PUSH_BACK_OAUTH_ID:-$BITBUCKET_DEFAULT_OAUTH_ID}
 GIT_PUSH_BACK_OAUTH_SECRET=${GIT_PUSH_BACK_OAUTH_SECRET:-$BITBUCKET_DEFAULT_OAUTH_SECRET}
+GIT_PUSH_BACK_USER=${GIT_PUSH_BACK_USER:-$BITBUCKET_DEFAULT_BOT_USER}
+GIT_PUSH_BACK_PASS=${GIT_PUSH_BACK_PASS:-$BITBUCKET_DEFAULT_BOT_PASS}
 GIT_PUSH_BACK_REPO_OWNER=${GIT_PUSH_BACK_REPO_OWNER:-$BITBUCKET_REPO_OWNER}
 GIT_PUSH_BACK_REPO_SLUG=${GIT_PUSH_BACK_REPO_SLUG:-$BITBUCKET_REPO_SLUG}
 NPM_REGISTRY_URL=${NPM_REGISTRY_URL:-$NPM_DEFAULT_REGISTRY_URL}
@@ -18,11 +20,14 @@ if [ "$NPM_REGISTRY_URL" != "" ]; then
   npm-cli-login -u "$NPM_REGISTRY_USER" -p "$NPM_REGISTRY_PW" -r "https://$NPM_REGISTRY_URL" -e "$NPM_REGISTRY_EMAIL" --quotes --config-path=/root/.npmrc
 fi
 
-echo "Requesting oAuth token..."
-export access_token=$(curl -s -X POST -u "${GIT_PUSH_BACK_OAUTH_ID}:${GIT_PUSH_BACK_OAUTH_SECRET}" \
-	https://bitbucket.org/site/oauth2/access_token \
-	-d grant_type=client_credentials -d scopes="repository"| jq --raw-output '.access_token')
-git remote set-url origin "https://x-token-auth:${access_token}@bitbucket.org/${GIT_PUSH_BACK_REPO_OWNER}/${GIT_PUSH_BACK_REPO_SLUG}"
+if [ "$GIT_PUSH_BACK_PASS" == "" ]; then
+  echo "Requesting oAuth token..."
+  GIT_PUSH_BACK_USER="x-token-auth"
+  GIT_PUSH_BACK_PASS=$(curl -s -X POST -u "${GIT_PUSH_BACK_OAUTH_ID}:${GIT_PUSH_BACK_OAUTH_SECRET}" \
+    https://bitbucket.org/site/oauth2/access_token \
+    -d grant_type=client_credentials -d scopes="repository"| jq --raw-output '.access_token')
+fi
+git remote set-url origin "https://${GIT_PUSH_BACK_USER}:${GIT_PUSH_BACK_PASS}@bitbucket.org/${GIT_PUSH_BACK_REPO_OWNER}/${GIT_PUSH_BACK_REPO_SLUG}"
 
 cd $RELEASE_DIR
 
