c78be9bf8eed65a47957ca88e3307e38e9b98260
Arni <toarni@gmail.com>
2025-05-16 18:40:26 +0200

    feat: adding a new image which starts and holds our test env in the pipelines

---
+++ test-environment/setup-test-env-and-start-compose.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Prepare environment variables..."
+TEST_ENV_IMAGE_LOAD_PATH=${TEST_ENV_IMAGE_LOAD_PATH:-""}
+TEST_ENV_COMPOSE_FILE_PATH=${TEST_ENV_COMPOSE_FILE_PATH:-"./docker-compose.yml"}
+
+if [ "$TEST_ENV_IMAGE_LOAD_PATH" != "" ] && [ -f "$TEST_ENV_IMAGE_LOAD_PATH" ]; then
+  echo "Loading image from file..."
+  docker load --input $TEST_ENV_IMAGE_LOAD_PATH
+fi
+
+echo "Starting the compose"
+docker-compose up -d -f "${TEST_ENV_COMPOSE_FILE_PATH}"
+
+echo "Sleep for some time to let the compose settle"
+sleep 25
