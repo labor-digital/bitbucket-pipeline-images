217083f71bab67b7108ad9260656b12397cf9f70
Arni <toarni@gmail.com>
2025-05-16 18:13:57 +0200

    fix: adding save and load docker image logic to our build and push image

--- build-docker-and-push-ecr/build-docker-and-push-ecr.sh
+++ build-docker-and-push-ecr/build-docker-and-push-ecr.sh
@@ -7,6 +7,8 @@ BUILD_AND_PUSH_REGION=${BUILD_AND_PUSH_REGION:-$AWS_ECR_DEFAULT_REGION}
 BUILD_AND_PUSH_DOCKER_FILE=${BUILD_AND_PUSH_DOCKER_FILE:-"Dockerfile"}
 BUILD_AND_PUSH_BUILD=${BUILD_AND_PUSH_BUILD:-"yes"}
 BUILD_AND_PUSH_PUSH=${BUILD_AND_PUSH_PUSH:-"yes"}
+BUILD_AND_PUSH_LOAD_PATH=${BUILD_AND_PUSH_LOAD_PATH:-""}
+BUILD_AND_PUSH_SAVE_PATH=${BUILD_AND_PUSH_SAVE_PATH:-""}
 
 # Extracts the base repository url from the image
 EXTRACTED_DOCKER_URL=$(echo $BUILD_AND_PUSH_IMAGE| cut -d'/' -f 1)
@@ -23,6 +25,11 @@ fi
 
 BUILD_AND_PUSH_TAG=${TAG:-"${EXTRACTED_TAG:-"DEFAULT_TAG"}"}
 BUILD_AND_PUSH_REPO=${BUILD_AND_PUSH_IMAGE%":$BUILD_AND_PUSH_TAG"}
+
+if [ "$BUILD_AND_PUSH_LOAD_PATH" != "" ] && [ -f "$BUILD_AND_PUSH_LOAD_PATH" ]; then
+  echo "Loading image from file..."
+  docker load --input $BUILD_AND_PUSH_LOAD_PATH
+fi
 
 if [ "$BUILD_AND_PUSH_BUILD" == "yes" ]; then
   echo "Building docker build-args out of .env.template"
@@ -42,4 +49,10 @@ if [ "$BUILD_AND_PUSH_PUSH" == "yes" ]; then
   echo "Pushing image..."
   docker push $BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG
   docker push $BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG_LATEST
+fi
+
+if [ "$BUILD_AND_PUSH_SAVE_PATH" != "" ]; then
+  echo "Saving image for later use..."
+  rm -rf $BUILD_AND_PUSH_SAVE_PATH
+  docker save --output $BUILD_AND_PUSH_SAVE_PATH $BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG
 fi
