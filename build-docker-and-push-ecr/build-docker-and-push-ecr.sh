1c22a72f84284d07a8f5ab15b980bf1bb61045d7
Arni <toarni@gmail.com>
2025-05-16 14:42:13 +0200

    fix: removing mysql-wait-and-import and some little fixes for docker-build

--- build-docker-and-push-ecr/build-docker-and-push-ecr.sh
+++ build-docker-and-push-ecr/build-docker-and-push-ecr.sh
@@ -5,17 +5,11 @@ BUILD_AND_PUSH_TAG_LATEST=${BUILD_AND_PUSH_TAG_LATEST:-"latest"}
 BUILD_AND_PUSH_IMAGE=${BUILD_AND_PUSH_IMAGE:-"$AWS_ECR_DEFAULT_URL/$PROJECT_NAME:$AWS_ECR_DEFAULT_TAG_PREFIX$BITBUCKET_COMMIT"}
 BUILD_AND_PUSH_REGION=${BUILD_AND_PUSH_REGION:-$AWS_ECR_DEFAULT_REGION}
 BUILD_AND_PUSH_DOCKER_FILE=${BUILD_AND_PUSH_DOCKER_FILE:-"Dockerfile"}
+BUILD_AND_PUSH_BUILD=${BUILD_AND_PUSH_BUILD:-"yes"}
+BUILD_AND_PUSH_PUSH=${BUILD_AND_PUSH_PUSH:-"yes"}
 
 echo "Performing AWS ECR login... ($BUILD_AND_PUSH_REGION)"
 eval $(aws ecr get-login --region $BUILD_AND_PUSH_REGION --no-include-email)
-
-echo "Building docker build-args out of .env.template"
-out=""
-for i in `cat .env.template`
-do
-	echo "ARG: $i"
-    out="$out --build-arg $i"
-done
 
 # Extracts the repository name from the image, so we can  push it with all tags
 EXTRACTED_TAG=$(echo $BUILD_AND_PUSH_IMAGE| cut -d':' -f 2)
@@ -27,10 +21,22 @@ fi
 BUILD_AND_PUSH_TAG=${TAG:-"${EXTRACTED_TAG:-"DEFAULT_TAG"}"}
 BUILD_AND_PUSH_REPO=${BUILD_AND_PUSH_IMAGE%":$BUILD_AND_PUSH_TAG"}
 
-echo "Start building of image ($BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG)..."
-out=$(echo $out|tr -d '\n'|tr -d '\r')
-docker build -f $BUILD_AND_PUSH_DOCKER_FILE -t $BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG -t $BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG_LATEST $out .
+if [ "$BUILD_AND_PUSH_BUILD" == "yes" ]; then
+  echo "Building docker build-args out of .env.template"
+  out=""
+  for i in `cat .env.template`
+  do
+    echo "ARG: $i"
+      out="$out --build-arg $i"
+  done
 
-echo "Pushing image..."
-docker push $BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG
-docker push $BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG_LATEST
+  echo "Start building of image ($BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG)..."
+  out=$(echo $out|tr -d '\n'|tr -d '\r')
+  docker build -f $BUILD_AND_PUSH_DOCKER_FILE -t $BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG -t $BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG_LATEST $out .
+fi
+
+if [ "$BUILD_AND_PUSH_PUSH" == "yes" ]; then
+  echo "Pushing image..."
+  docker push $BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG
+  docker push $BUILD_AND_PUSH_REPO:$BUILD_AND_PUSH_TAG_LATEST
+fi
