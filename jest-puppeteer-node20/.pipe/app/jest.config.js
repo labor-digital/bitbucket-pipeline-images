a9a4825adf5e029cae338fc1d2b8fe50ffa33039
Arni <toarni@gmail.com>
2025-05-19 23:08:41 +0200

    feat: adding a teardown to our jest image which tries to call the bitbucket reports API

--- jest-puppeteer-node20/.pipe/app/jest.config.js
+++ jest-puppeteer-node20/.pipe/app/jest.config.js
@@ -3,5 +3,6 @@ const common = require('./jest.common.config.js')
 module.exports = {
     ...common,
 
-    testFailureExitCode: 1
+    testFailureExitCode: 1,
+    globalTeardown: "./global-teardown.js"
 };
