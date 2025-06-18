a0c7d4d2f29836cdd824a98b0a6c8ea313f30d24
Arni <toarni@gmail.com>
2025-06-18 17:40:06 +0200

    chore: just testing something

--- jest-puppeteer-node20/.pipe/app/jest.config.js
+++ jest-puppeteer-node20/.pipe/app/jest.config.js
@@ -3,6 +3,6 @@ const common = require('./jest.common.config.js')
 module.exports = {
     ...common,
 
-    testFailureExitCode: 1,
-    globalTeardown: "./global-teardown.js"
+    testFailureExitCode: 1//,
+    // globalTeardown: "./global-teardown.js"
 };
