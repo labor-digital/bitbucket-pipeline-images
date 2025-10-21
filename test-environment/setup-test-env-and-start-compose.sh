73610d7cba5c5300e22d083cd6958b5f024471a3
Arni <toarni@gmail.com>
2025-10-21 23:24:04 +0200

    fix: adding a little output in our pipelines test-env

--- test-environment/setup-test-env-and-start-compose.sh
+++ test-environment/setup-test-env-and-start-compose.sh
@@ -10,4 +10,4 @@ if [ "$TEST_ENV_IMAGE_LOAD_PATH" != "" ] && [ -f "$TEST_ENV_IMAGE_LOAD_PATH" ]; 
 fi
 
 echo "Starting the compose"
-docker-compose -f "${TEST_ENV_COMPOSE_FILE_PATH}" up -d --wait
+docker-compose -f "${TEST_ENV_COMPOSE_FILE_PATH}" up -d --wait --wait-timeout 120
