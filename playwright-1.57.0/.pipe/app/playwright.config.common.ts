e4ca240acf6d09ba702a6bcec9fbf5afecd9ebe9
Arni <toarni@gmail.com>
2026-01-23 12:54:54 +0100

    fix: adding a new playwright version 1.57.0

---
+++ playwright-1.57.0/.pipe/app/playwright.config.common.ts
@@ -0,0 +1,11 @@
+import base from './playwright.config.base';
+import { defineConfig } from '@playwright/test';
+export default defineConfig({
+  ...base,
+  
+  workers: 5,
+  reporter: [
+    ...base['reporter'],
+    ['/app/pipes-reporter.ts'],
+  ],
+});
